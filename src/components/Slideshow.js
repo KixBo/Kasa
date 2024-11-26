f23a94f0a03806da98e2c96c553a2bb9c953bc72
KixBo <killian.boyer@hotmail.fr>
2024-11-26 23:18:20 +0100

    Dynamic slideshow component
    Change to the slideshow component:
    - Hide the buttons and the index span when there is only one image.
    -State to change the image that is displayed when clicking on the previous and next buttons.
    - Condition for the carousel to be "infinite"

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -1,24 +1,43 @@
 import "../styles/Slideshow.scss";
 import nextbtn from "../assets/nextbtn.svg";
 import previousbtn from "../assets/previousbtn.svg";
+import { useState } from "react";
 
 function Slideshow({ pictures, title }) {
   // state (état, données)
+  const hidenElement = pictures.length === 1 && "hidden";
+
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   // comportements
+  const prevImage = () => {
+    if (currentIndex === 0) {
+      setCurrentIndex(pictures.length - 1);
+    } else {
+      setCurrentIndex(currentIndex - 1);
+    }
+  };
+
+  const nextImage = () => {
+    if (currentIndex === pictures.length - 1) {
+      setCurrentIndex(0);
+    } else {
+      setCurrentIndex(currentIndex + 1);
+    }
+  };
 
   // affichage (render)
   return (
     <>
       <div className="carousel">
-        <img src={pictures[0]} alt={`Photo du logement : ${title}`} className="carouselPicture" />
-        <button className="prevBtn">
+        <img src={pictures[currentIndex]} alt={`Logement : ${title}`} className="carouselPicture" />
+        <button onClick={prevImage} className={`prevBtn ${hidenElement}`}>
           <img src={previousbtn} alt="Bouton précédent" />
         </button>
-        <button className="nextBtn">
+        <button onClick={nextImage} className={`nextBtn ${hidenElement}`}>
           <img src={nextbtn} alt="Bouton suivant" />
         </button>
-        <span className="carouselIndex">1/4</span>
+        <span className={`carouselIndex ${hidenElement}`}>{`${currentIndex + 1}/${pictures.length}`}</span>
       </div>
     </>
   );
