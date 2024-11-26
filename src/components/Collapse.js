2b18c297fd65da896ad375ddba4df27ade567961
KixBo <killian.boyer@hotmail.fr>
2024-11-26 01:56:33 +0100

    Creating a collapse component and importing the component on the About page
    Creation of the collapse component and integration of 4 collapse in the About page by giving it the title and content in props

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -1,19 +1,25 @@
 import "../styles/Collapse.scss";
+import icon from "../assets/dropdownicon.svg";
 
-function Collapse(props) {
+function Collapse({ title, content }) {
   // state (état, données)
-  
-  // comportements
-  
-  // affichage (render)
-    return (
-      <>
-      
-        <div>
 
+  // comportements
+
+  // affichage (render)
+  return (
+    <>
+      <div className="collapseContainer">
+        <div className="collapseTitle">
+          <h3>{title}</h3>
+          <img src={icon} alt="Icone d'ouverture/fermeture de l'accordéon" />
         </div>
-      </>
-    );
-  }
-  
-  export default Collapse;
+        <div className="collapseContent">
+          <p>{content}</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Collapse;
