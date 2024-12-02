b9fee34c4115797b5743208e357fa45e89e93ea0
KixBo <killian.boyer@hotmail.fr>
2024-12-03 00:43:35 +0100

    Resonsive of the About.js page

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -2,7 +2,7 @@ import "./Collapse.scss";
 import icon from "../assets/dropdownicon.svg";
 import { useState } from "react";
 
-function Collapse({ title, content, className }) {
+function Collapse({ title, content, styleAboutCollapseTitle, styleAboutCollapseContent }) {
   // state (état, données)
   const [isOpen, setOpen] = useState(false);
 
@@ -11,20 +11,18 @@ function Collapse({ title, content, className }) {
   // affichage (render)
   return (
     <>
-      <div className={`collapseContainer ${className === "accommodationSheetStyle" ? "accommodationSheetStyle" : ""}`}>
-        <div className="collapseTitle">
-          <h3>{title}</h3>
-          <img
-            className={`icon ${isOpen && "rotation"}`}
-            onClick={() => {
-              setOpen(!isOpen);
-            }}
-            src={icon}
-            alt="Icone d'ouverture/fermeture de l'accordéon"
-          />
-        </div>
-        <div className={`collapseContent ${isOpen ? "visible" : "invisible"}`}>{content}</div>
+      <div className={`collapseTitle ${styleAboutCollapseTitle}`}>
+        <h3>{title}</h3>
+        <img
+          className={`icon ${isOpen && "rotation"}`}
+          onClick={() => {
+            setOpen(!isOpen);
+          }}
+          src={icon}
+          alt="Icone d'ouverture/fermeture de l'accordéon"
+        />
       </div>
+      <div className={`collapseContent ${isOpen ? "visible" : "invisible"} ${styleAboutCollapseContent}`}>{content}</div>
     </>
   );
 }
